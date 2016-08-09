3cc852db706467fe59c1f85de3ab909e8d29d626
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-08-09 08:47:02 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,35 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+    tmp = Genre.find_or_create_by(name: name)
+    self.genre = tmp 
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end    
+
+  def artist_name=(name)
+    tmp = Artist.find_or_create_by(name: name)
+    self.artist = tmp
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def note_contents=(notes)
+    notes.reject(&:empty?).each do |x|
+      tmp = Note.find_or_create_by(content:x)
+      self.notes << tmp
+    end
+  end
+
+  def note_contents
+    self.notes.collect { |x| x.content }
+  end
+
 end
 
